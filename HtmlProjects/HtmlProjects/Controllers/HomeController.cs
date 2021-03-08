4434289f691690fdb0cb4331f2c7f990fc87ea09
Cristal Zavala <czavala7133@southwestuniversity.edu>
2021-03-08 11:01:11 -0700

    this is module 8

---
+++ HtmlProjects/HtmlProjects/Controllers/HomeController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using HtmlProjects.Models;
+
+namespace HtmlProjects.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+        public IActionResult Register()
+        {
+            return View();
+
+        }
+        public IActionResult ShopperProfile()
+        {
+            return View();
+        }
+
+        public IActionResult SaveUser(string UsarName, String Password)
+        {
+            string myResult =
+                "The user is:" + UsarName + " and the password is:" + Password;
+            return Content(myResult);
+
+        }
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
